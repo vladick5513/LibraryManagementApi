23b71805fad10af7b2e4d541966f9da74b97057d
vlados <2006krickunow@gmail.com>
2025-03-15 12:15:46 +0300

    Add find_one_or_none method

--- app/dao/base.py
+++ app/dao/base.py
@@ -14,6 +14,14 @@ class BaseDAO:
     model = None
 
     @classmethod
+    async def find_one_or_none(cls, **filter_by):
+        async with async_session_factory() as session:
+            query = select(cls.model).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.scalar_one_or_none()
+
+
+    @classmethod
     async def find_one_or_none_by_id(cls, data_id: int):
         async with async_session_factory() as session:
             query = select(cls.model).filter_by(id=data_id)
