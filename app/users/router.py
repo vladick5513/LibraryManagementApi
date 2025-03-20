b538e1e802cd2144d112a1dd78b51083848dee94
vlados <2006krickunow@gmail.com>
2025-03-20 17:39:27 +0300

    Add login endpoint

--- app/users/router.py
+++ app/users/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response, Depends
 from starlette import status
 
 from app.users.auth import get_password_hash
@@ -16,3 +16,14 @@ async def register_user(user_data: SUserRegister):
     user_dict["password"] = get_password_hash(user_data.password)
     await UsersDAO.create(**user_dict)
     return {"message": f'Вы успешно зарегистрированы!'}
+
+@router.post("/login/")
+async def auth_user(response: Response, user_data: SUserAuth):
+    check = await authenticate_user(email=user_data.email, password=user_data.password)
+    if check is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Неверная почта или пароль")
+    access_token = create_access_token({"sub": str(check.id)})
+    response.set_cookie(key="user_access_token", value=access_token,  httponly=True)
+    return {"access_token": access_token, "refresh_token": None}
+
